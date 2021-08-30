c1444033d606ff2b5dd47c9b080a87265f51277b
MANISHUKIRADE <ukirademanish9797@gmail.com>
2021-08-31 01:21:28 +0530

    feat:added cors

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var cors = require('cors');
 const productRouter = require('./routes/product');
 var app = express();
 
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors())
 app.use('/product',productRouter)
 
 module.exports = app;
